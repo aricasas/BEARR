982e5bb8452d50012f50f01aef2869a1c4f954e9
bigwhoman <bigwhoman@debian.bigwhoman>
2025-10-06 18:07:05 -0400

    added some tests plus small io

--- src/sst.rs
+++ src/sst.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs, io,
     ops::RangeInclusive,
     path::{Path, PathBuf},
 };
@@ -6,13 +7,30 @@ use std::{
 use crate::DBError;
 
 /// A handle to an SST of a database
+#[derive(Debug)]
 pub struct SST {
     filename: PathBuf,
 }
 
 impl SST {
+    /*
+     * Create an SST table to store contents on disk
+     * */
     pub fn create(key_values: Vec<(u64, u64)>, path: &Path) -> Result<SST, DBError> {
-        todo!()
+        println!("new file ---> {}", path.display());
+        let path: PathBuf = path.to_path_buf();
+        let sst = SST {
+            filename: path.clone(),
+        };
+        match fs::write(&path, "hello world") {
+            Ok(_) => {}
+            Err(e) => {
+                println!("failed to write : {}", e);
+                return Err(DBError);
+            }
+        }
+
+        Ok(sst)
     }
 
     pub fn open(path: &Path) -> Result<SST, DBError> {
@@ -28,11 +46,42 @@ impl SST {
     }
 }
 
+/* SST itterator */
 pub struct SSTIter {}
 impl Iterator for SSTIter {
     type Item = (u64, u64);
 
     fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+/* Tests for SSTs */
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_sst() {
+        let path = Path::new("/home/bigwhomann/file.txt");
+        let sst = SST::create(vec![], path);
+        if let Ok(value) = sst {
+            println!("{}", value.filename.display());
+        }
+    }
+
+    #[test]
+    fn test_write_to_sst() {
+        todo!()
+    }
+
+    #[test]
+    fn test_read_from_sst() {
+        todo!()
+    }
+
+    #[test]
+    fn test_scan_sst() {
         todo!()
     }
 }
