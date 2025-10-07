6d5bb6ba0b9d00562ce302f76ff278f810b814c4
bigwhoman <bigwhoman@debian.bigwhoman>
2025-10-07 12:46:27 -0400

    Completed SST create

--- src/sst.rs
+++ src/sst.rs
@@ -2,11 +2,13 @@
 
 use std::{
     fs, io,
+    io::Write,
     ops::RangeInclusive,
     path::{Path, PathBuf},
 };
 
 use crate::DBError;
+use serde::{Deserialize, Serialize};
 
 /// A handle to an SST of a database
 #[derive(Debug)]
@@ -21,14 +23,39 @@ impl SST {
     pub fn create(key_values: Vec<(u64, u64)>, path: &Path) -> Result<SST, DBError> {
         println!("new file ---> {}", path.display());
         let path: PathBuf = path.to_path_buf();
+
+        /* TODO : A less expensive way to check if file exists??
+         * */
+
+        let mut file = match fs::File::create_new(&path) {
+            Ok(file) => file,
+            Err(e) => {
+                println!("failed to create : {}", e);
+                return Err(DBError::IOError(e.to_string()));
+            }
+        };
+
         let sst = SST {
             filename: path.clone(),
         };
-        match fs::write(&path, "hello world") {
-            Ok(_) => {}
+
+        /* Serialize the vector */
+        let bytes = match bincode::serialize(&key_values) {
+            Ok(bytes) => bytes,
+            Err(e) => {
+                println!("Serialization Error : {}", e);
+                return Err(DBError::IOError(e.to_string()));
+            }
+        };
+
+        /* Write to file and make sure the write is flushed to disk */
+        match file.write_all(&bytes) {
+            Ok(n) => {
+                file.sync_all();
+            }
             Err(e) => {
                 println!("failed to write : {}", e);
-                return Err(DBError);
+                return Err(DBError::IOError(e.to_string()));
             }
         }
 
@@ -64,11 +91,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_create_sst() {
-        let path = Path::new("/home/bigwhomann/file.txt");
+    fn test_problematic_create_sst() {
+        let path = Path::new("/xyz/abc/file.txt");
         let sst = SST::create(vec![], path);
+        assert!(sst.is_err());
+
+        let path = Path::new("./db/SSTe.txt");
+        let sst = SST::create(vec![], path);
+        let sst = SST::create(vec![], path);
+        assert!(sst.is_err());
+    }
+
+    #[test]
+    fn test_create_sst() {
+        let path = Path::new("./db/SST");
+        let sst = SST::create(vec![], path);
+        assert!(!sst.is_err());
         if let Ok(value) = sst {
-            println!("{}", value.filename.display());
+            println!("foile {}", value.filename.display());
         }
     }
 
