f686cef77960ee00e03b61edc4df14ae460901a0
ari.casas.nassar@mail.utoronto.ca <ari.casas.nassar@mail.utoronto.ca>
2025-10-22 00:54:58 -0400

    New interface for Eviction that allows choosing eviction victim Mock FIFO implementation (untested)

--- src/eviction.rs
+++ src/eviction.rs
@@ -1,70 +1,71 @@
-use std::collections::VecDeque;
-
 use crate::DbError;
-
-const NULL: usize = usize::MAX;
 
 #[derive(Debug, Clone, Copy)]
 pub enum EvictionId {
-    Mock,
-    In(usize),
-    Out(usize),
-    M(usize),
+    // we don't need any id/pointer in FIFO
+    MockFifo,
 }
 
 #[derive(Debug)]
-pub struct Eviction<T> {
-    mock_queue: VecDeque<T>,
-    a1_in: Queue<T>,
-    a1_out: Queue<T>,
-    a_m: Queue<T>,
-    k_in: usize,
-    k_out: usize,
+pub struct Eviction<T: Clone> {
+    mock_queue: Vec<T>,
 }
 
 impl<T: Clone> Eviction<T> {
     pub fn new(capacity: usize) -> Result<Self, DbError> {
-        let k_in = capacity / 4; // ~25%
-        let k_out = capacity / 2; // ~50%
+        let _ = capacity;
 
         Ok(Self {
-            mock_queue: VecDeque::new(),
-            a1_in: Queue::new(k_in)?,
-            a1_out: Queue::new(k_out)?,
-            a_m: Queue::new(capacity)?,
-            k_in,
-            k_out,
+            mock_queue: Vec::new(),
         })
     }
 
-    pub fn evict(&mut self) -> T {
-        self.mock_queue.pop_front().unwrap()
+    pub fn choose_victim(self) -> VictimChooser<T> {
+        VictimChooser::new(self)
     }
 
     pub fn insert_new(&mut self, entry: T) -> EvictionId {
-        self.mock_queue.push_back(entry);
-        EvictionId::Mock
+        self.mock_queue.push(entry);
+        EvictionId::MockFifo
     }
 
-    pub fn touch(&mut self, id: EvictionId) {}
+    pub fn touch(&mut self, id: EvictionId) {
+        let _ = id;
+    }
 }
 
-#[derive(Debug)]
-struct Queue<T> {
-    inner: Vec<T>,
-    front: usize,
-    back: usize,
+pub struct VictimChooser<T: Clone> {
+    eviction: Eviction<T>,
+    chosen_idx: Option<usize>,
 }
 
-impl<T> Queue<T> {
-    fn new(capacity: usize) -> Result<Self, DbError> {
-        let mut inner = Vec::new();
-        inner.try_reserve_exact(capacity)?;
+impl<T: Clone> VictimChooser<T> {
+    /// Confirm choice of victim to evict. The last victim returned when calling .next() is evicted.
+    ///
+    /// If .next() was never called, or if .next() was called until it returns None, no victim is evicted.
+    pub fn confirm(mut self) -> Eviction<T> {
+        if let Some(idx) = self.chosen_idx
+            && idx < self.eviction.mock_queue.len()
+        {
+            self.eviction.mock_queue.remove(idx);
+        }
 
-        Ok(Self {
-            inner,
-            front: NULL,
-            back: NULL,
-        })
+        self.eviction
+    }
+    fn new(eviction: Eviction<T>) -> Self {
+        Self {
+            eviction,
+            chosen_idx: None,
+        }
+    }
+}
+impl<T: Clone> Iterator for VictimChooser<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next = self.chosen_idx.map(|idx| idx + 1).unwrap_or(0);
+        self.chosen_idx = Some(next);
+
+        self.eviction.mock_queue.get(next).cloned()
     }
 }
