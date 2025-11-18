575ab51ce12ce7229595845890c0293da07adbb7
bigwhoman <bigwhoman@debian.bigwhoman>
2025-11-18 17:47:24 -0500

    added bloom filter test

--- src/bloom_filter.rs
+++ src/bloom_filter.rs
@@ -4,17 +4,14 @@ use crate::hash::HashFunction;
 
 #[derive(Debug, Clone)]
 pub struct BloomFilter {
-    pub hash_functions: Vec<HashFunction>,
-    bits: BitVec, // this is from a crate, or implement your own if you want
+    pub hash_functions: Vec<HashFunction>, // Bloom filter's hash functions
+    bits: BitVec,                          // Bloom filter's bitmap
 }
 
 impl BloomFilter {
+    /// Create an empty bloom filter having the number of entries and bits per each entry
     pub fn empty(n_entries: usize, bits_per_entry: usize) -> Self {
         // Calculate how many hash functions it needs to be optimal: bits_per_entry * ln(2)
-        // Careful because slides have error https://piazza.com/class/mf0gdjv1iov3n/post/77
-
-        // TODO:Might want to round capacity up to multiple of PAGE_SIZE
-
         let bits = BitVec::from_elem(n_entries * bits_per_entry, false);
         let num_hashes = (bits_per_entry as f32 * f32::ln(2.0)).ceil() as usize;
 
@@ -26,6 +23,7 @@ impl BloomFilter {
         }
     }
 
+    /// Turn a vector of bits into bloom filter by having the number of hashes
     pub fn from_bytes(filter_bytes: &[u8], num_hashes: usize) -> Self {
         let bits_offset = num_hashes * size_of::<HashFunction>();
         let hash_functions: &[HashFunction] = bytemuck::cast_slice(&filter_bytes[0..bits_offset]);
@@ -37,6 +35,7 @@ impl BloomFilter {
         }
     }
 
+    /// Turn filter into a vector of bytes
     pub fn turn_to_bytes(&self) -> Vec<u8> {
         let hash_bytes = bytemuck::cast_slice(&self.hash_functions);
         let mut hash_bytes = hash_bytes.to_vec();
@@ -44,21 +43,7 @@ impl BloomFilter {
         hash_bytes
     }
 
-    pub fn from_hashes_and_bits(hash_functions: Vec<HashFunction>, bits: Vec<u8>) -> Self {
-        let bits = BitVec::from_bytes(&bits);
-
-        Self {
-            hash_functions,
-            bits,
-        }
-    }
-
-    // TODO change this interface to return whatever you need to store in the file
-    // This was just a guess to how it could work
-    pub fn into_hashes_and_bits(self) -> (Vec<HashFunction>, Vec<u8>) {
-        (self.hash_functions, self.bits.to_bytes())
-    }
-
+    /// Insert a key into the bloom filter
     pub fn insert(&mut self, key: u64) {
         let bitmap_len = self.bits.len();
         let hash_functions = &self.hash_functions;
@@ -70,6 +55,7 @@ impl BloomFilter {
         }
     }
 
+    /// Search for a key inside the bloom filter
     pub fn query(&self, key: u64) -> bool {
         let bitmap_len = self.bits.len();
         for hashed_index in self
@@ -82,5 +68,32 @@ impl BloomFilter {
             }
         }
         true
+    }
+}
+
+/* Tests for SSTs */
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::iter::repeat_with;
+
+    #[test]
+    /// Test to see if bloom filter works correctlly
+    /// need to make sure it doesn't return any false negatives
+    pub fn bloom_filter_test() {
+        let entries_num = 10;
+        let bits_per_entry = 20;
+        let num_elements = 100;
+        let entries: Vec<u64> = repeat_with(|| fastrand::u64(..))
+            .take(num_elements)
+            .collect();
+        let mut filter = BloomFilter::empty(entries_num, bits_per_entry);
+        for entry in &entries {
+            filter.insert(*entry);
+        }
+
+        for entry in &entries {
+            assert!(filter.query(*entry));
+        }
     }
 }
